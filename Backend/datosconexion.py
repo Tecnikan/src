dffd9eca15e5b240969031d87ca44bbbdcc62fbb
Tecnikan <nes@tecnikan.ar>
2024-07-14 15:01:26 -0300

    first commit

---
+++ Backend/datosconexion.py
@@ -0,0 +1,7 @@
+host='Nestor380.mysql.pythonanywhere-services.com'
+user='Nestor380'
+password='Jeremias33'
+database='Nestor380$default'
+
+# colocar aqui los datos de conexion a la base de datos
+# luego renombrar el archivo a datosconexion.py
---
+++ js/script.js
@@ -0,0 +1,49 @@
+/* Mostrar fecha en el header del sitio. */
+const daysOfWeek = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+];
+const date = new Date();
+const dayName = daysOfWeek[date.getDay()];
+document.getElementById(
+  "fecha_actual"
+).innerHTML = `${dayName}, ${date.toLocaleDateString()}`;
+
+/* Para el slider de fotos del index * Nestor */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
